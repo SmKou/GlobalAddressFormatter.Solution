b8e532e427b2d9446961045f22e223ea6b6ba135
SmKou <66178856+SmKou@users.noreply.github.com>
2023-08-21 08:28:33 -0700

    Initial

---
+++ FormatterClient/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FormatterClient.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
