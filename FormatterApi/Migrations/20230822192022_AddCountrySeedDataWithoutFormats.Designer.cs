7cabea5e523a8ebaecf2e0e2a3aaf7fc8cbdaca6
Allie Zhao <allie.k.zhao@gmail.com>
2023-08-22 12:21:58 -0700

    Add country seed data without formats, migrate

--- FormatterApi/Migrations/20230822192022_AddCountrySeedDataWithoutFormats.Designer.cs
+++ FormatterApi/Migrations/20230822192022_AddCountrySeedDataWithoutFormats.Designer.cs
@@ -2,6 +2,7 @@
 using FormatterApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FormatterApi.Migrations
 {
     [DbContext(typeof(FormatterApiContext))]
-    partial class FormatterApiContextModelSnapshot : ModelSnapshot
+    [Migration("20230822192022_AddCountrySeedDataWithoutFormats")]
+    partial class AddCountrySeedDataWithoutFormats
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- FormatterApi/Models/FormatterApiContext.cs
+++ FormatterApi/Models/FormatterApiContext.cs
@@ -8,4 +8,17 @@ public class FormatterApiContext : DbContext
     public DbSet<Field> Fields { get; set; }
     public DbSet<FormatField> FormatFields { get; set; }
     public FormatterApiContext(DbContextOptions<FormatterApiContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Country>()
+            .HasData(
+                new Country { CountryId = 1, CountryCode = "AUS", CountryName = "Australia", Formats = new List<Format>() },
+                new Country { CountryId = 2, CountryCode = "BDG", CountryName = "Bangladesh", Formats = new List<Format>() },
+                new Country { CountryId = 3, CountryCode = "CHL", CountryName = "Chile", Formats = new List<Format>() },
+                new Country { CountryId = 4, CountryCode = "GRC", CountryName = "Greece", Formats = new List<Format>() },
+                new Country { CountryId = 5, CountryCode = "IND", CountryName = "India", Formats = new List<Format>() },
+                new Country { CountryId = 6, CountryCode = "USA", CountryName = "United States", Formats = new List<Format>() }
+            );
+    }
 }
