6068ffa4e998c43802bcbc33575b4ecf57c63693
SmKou <66178856+SmKou@users.noreply.github.com>
2023-08-22 23:07:21 -0700

    Add dynamic fields and formatting (req. integration testing with api)

--- FormatterClient/Models/Format.cs
+++ FormatterClient/Models/Format.cs
@@ -27,7 +27,7 @@ public class Format
                 new Format
                 {
                     FormatName = "GeneralWithStreet",
-                    Pattern = "{business}\n{addressee}\n{street-number} {street-name}\n{locality}, {state}, {postal-code}",
+                    Pattern = "{:business}\n{addressee}\n{street-number} {street-name}\n{locality}, {state}, {postal-code}",
                     Description = "A general format for the country of Australia that takes a street number and street name. The locality, state and postal code are required. Specifying the name of a business is optional. Addressee is the name of the recipient.",
                     Fields = new List<string>
                     {
@@ -43,7 +43,7 @@ public class Format
                 new Format
                 {
                     FormatName = "GeneralWithPOBox",
-                    Pattern = "{business}\n{addressee}\n{po-box}\n{locality}, {state}, {postal-code}",
+                    Pattern = "{:business}\n{addressee}\n{po-box}\n{locality}, {state}, {postal-code}",
                     Description = "A general format for the country of Australia that takes a PO Box. The locality, state and postal code are required. Specifying the name of a business is optional. Addressee is the name of the recipient.",
                     Fields = new List<string>
                     {
@@ -58,7 +58,7 @@ public class Format
                 new Format
                 {
                     FormatName = "GeneralWithLockedBagNumber",
-                    Pattern = "{business}\n{addressee}\n{locked-bag-number}\n{locality}, {state}, {postal-code}",
+                    Pattern = "{:business}\n{addressee}\n{locked-bag-number}\n{locality}, {state}, {postal-code}",
                     Description = "A general format for the country of Australia that takes a locked bag number. The locality, state and postal code are required. Specifying the name of a business is optional. Addressee is the name of the recipient.",
                     Fields = new List<string>
                     {
@@ -77,7 +77,7 @@ public class Format
                 new Format
                 {
                     FormatName = "Urban",
-                    Pattern = "{addressee}\n{addressee-details}\n{unit-number}, {building-name}\n{street-name} {street-number}\n{town}+{postal-code}\n{country}",
+                    Pattern = "{addressee}\n{:addressee-details}\n{:unit-number}, {:building-name}\n{street-name} {street-number}\n{town}+{postal-code}\n{country}",
                     Description = "The urban format for the country of Bangladesh. Addressee details are optional. Country is only needed for mailing internationally. The unit number and name of the building need only be provided if applicable. Otherwise, the name of the addressee, or recipient of the mail or package, the street name and street number, the town, and postal code are required.",
                     Fields = new List<string>
                     {
@@ -94,7 +94,7 @@ public class Format
                 new Format
                 {
                     FormatName = "Rural",
-                    Pattern = "{addressee}\n{addressee-details}\nVillage: {village}\nP.O.:{post-office}\nThana: {thana-name}\n{country}",
+                    Pattern = "{addressee}\n{:addressee-details}\nVillage: {village}\nP.O.:{post-office}\nThana: {thana-name}\n{country}",
                     Description = "The rural format for the couontry of Bangladesh. Addressee details are optional. Country is only needed for mailing internationally. The name of the addressee, or recipient of the mail or package, the village, post office and thana are required. A thana, meaning police station, is the district of a police station's jurisdiction.",
                     Fields = new List<string>
                     {
